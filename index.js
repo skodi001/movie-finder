a363efaf4765bfac6d148924c5352e1d3f44ba5a
Shokhmurod <shokhpjwstk@gmail.com>
2022-04-22 23:53:43 +0200

    delaying the search input has been worked out. The key should be updated

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hi there!");
+const fetchData = async (searchTerm) => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "d9835cc5",
+      s: searchTerm,
+    },
+  });
+
+  console.log(response.data);
+};
+
+const input = document.querySelector("input");
+input.addEventListener("input", (event) => {
+  fetchData(event.target.value);
+});
